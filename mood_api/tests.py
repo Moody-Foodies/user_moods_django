ea77b8494f05aa09c209f9abdfe91c374aa4dff6
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-23 14:58:32 -0400

    test model Mood

--- mood_api/tests.py
+++ mood_api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Mood
 
-# Create your tests here.
+class MoodModelTest(TestCase):
+  def test_mood_model_exists(self):
+    moods = Mood.objects.count()
+
+    self.assertEqual(moods, 0)
