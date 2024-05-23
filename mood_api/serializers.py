5f898e90f8ebb6e5136520c2e42f5e6b86b48d49
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-23 11:43:03 -0400

    serializers

---
+++ mood_api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Mood
+class MoodSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Mood
+    fields = ('user', 'mood', 'date')
