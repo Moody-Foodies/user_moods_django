23ea00e7954aeaab433768afa0cf632089f6c77e
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-23 11:24:19 -0400

    feat: model

--- mood/urls.py
+++ mood/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from mood_api import urls as api_urls
+from mood_api import urls as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
-    # path('mood/', include(api_urls))
+    path('api-auth/', include('rest_framework.urls')),
+    path('moods/', include(api_urls)),
 ]
--- mood_api/models.py
+++ mood_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Mood(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  mood = models.IntegerField(blank = False)
+  date = models.DateTimeField(auto_now = True, blank = False)
+  created = models.DateTimeField(auto_now = True, blank = True)
+
+  def __str__(self):
+    return '%s, %s, %s' % (self.user, self.mood, self.date)
